d46038427bedc567121e4e685121e9d9680de011
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-09 18:34:38 +0100

    Working with Booleans

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean): number | void {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
 
 const number1 = 5;
 const number2 = 2.8;
 
-const result = add(number1, number2);
+const printResult = true;
 
-console.log(result);
+add(number1, number2, printResult);
