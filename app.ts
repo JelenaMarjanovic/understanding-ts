a02d9eb448885260d85b2f3a609bbb53e3c1c545
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-11 00:36:02 +0100

    Function Return Types & "void"

--- app.ts
+++ app.ts
@@ -1,61 +1,9 @@
-type Combinable = number | string;
-
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-): Combinable {
-  let result: Combinable;
-
-  if (
-    (typeof input1 === 'number' && typeof input2 === 'number') ||
-    resultConversion === 'as-number'
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result;
-
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
-
-type User = {
-  name: string;
-  age: number;
-};
-
-let u1: User;
-u1 = {
-  name: 'Max',
-  age: 30
-};
-
-function greet(user: User): void {
-  console.log('Hi, I am ' + user.name);
+function printResult(num: number) {
+  console.log('Result: ' + num);
 }
 
-function isOlder(user: User, checkAge: number): boolean {
-  return checkAge > user.age;
-}
-
-greet(u1);
-
-const checkIfOlder = isOlder(u1, 36);
-console.log('Is older?', checkIfOlder);
+printResult(add(5, 8));
