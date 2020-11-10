6391cc340fe45b92ae4b637a8a166cd3c44398e8
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-11 00:48:31 +0100

    Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,12 @@ function printResult(num: number) {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+
+  cb(result);
+}
+
 printResult(add(5, 8));
 
 // let combineValues: Function;
@@ -14,3 +20,7 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 9));
+
+addAndHandle(3, 4, (result) => {
+  console.log('Callback: ', result);
+});
