0706cf3130a5067c456a2c5c78ec0a3032b0273b
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-11 02:45:46 +0100

    Shorthand Initialization

--- app.ts
+++ app.ts
@@ -1,13 +1,12 @@
 class Department {
-  name: string;
+  // private id: string;
+  // name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
-  }
+  constructor(private id: string, public name: string) {}
 
   describe(this: Department) {
-    console.log('Department: ' + this.name);
+    console.log(`Department: (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -20,7 +19,7 @@ class Department {
   }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
 
 const newEmployees = ['Max', 'Manuel', 'Anna'];
 newEmployees.forEach((employee) => accounting.addEmployee(employee));
@@ -31,6 +30,7 @@ accounting.describe();
 accounting.printEmployeeInformation();
 
 // const accountingCopy = {
+//   id: 'd2',
 //   name: 'DUMMY',
 //   employees: [],
 //   describe: accounting.describe,
