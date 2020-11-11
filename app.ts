b3d3d76a2a09c893d9b7971ed1c7db3ef077d6d7
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-11 14:23:38 +0100

    Inheritance

--- app.ts
+++ app.ts
@@ -23,23 +23,65 @@ class Department {
   }
 }
 
-const accounting = new Department('d1', 'Accounting');
+class ITDepartment extends Department {
+  // admins: string[];
+
+  // constructor(id: string, admins: string[]) {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+
+    // this.admins =  admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  // private reports: string[];
+
+  // constructor(id: string, reports: string[]) {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports(this: AccountingDepartment) {
+    console.log(this.reports);
+  }
+}
+
+// const department = new Department('d1', 'Accounting');
+
+const it = new ITDepartment('d3', ['Max']);
 
 const newEmployees = ['Max', 'Manuel', 'Anna'];
-newEmployees.forEach((employee) => accounting.addEmployee(employee));
-// accounting.employees[2] = 'Anna'; // BAD PRACTICE, avoiding with using private access modifier
+newEmployees.forEach((employee) => it.addEmployee(employee));
+// it.employees[2] = 'Anna'; // BAD PRACTICE, avoiding with using private access modifier
 
-accounting.describe();
+it.describe();
 
-accounting.printEmployeeInformation();
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment('d4', []);
+
+accounting.addEmployee('Henry');
+
+accounting.addReport('Something went wrong...');
+
+accounting.printReports();
+
+console.log(accounting);
 
 // const accountingCopy = {
 //   id: 'd2',
 //   name: 'DUMMY',
 //   employees: [],
-//   describe: accounting.describe,
-//   addEmployee: accounting.addEmployee,
-//   printEmployeeInformation: accounting.printEmployeeInformation
+//   describe: department.describe,
+//   addEmployee: department.addEmployee,
+//   printEmployeeInformation: department.printEmployeeInformation
 // };
 
 // accountingCopy.describe();
