43410a92952fca69a21f524fd23ff5c251c1b46a
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-11 01:06:07 +0100

    The "never" Type

--- app.ts
+++ app.ts
@@ -12,3 +12,9 @@ userName = someVar;
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred!', 500);
