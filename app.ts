970a91f453b6ffa8ef3e1c9fb70789e4d55cfd07
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-12 01:20:55 +0100

    Optional Parameters & Properties

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -8,21 +9,30 @@ interface Greetable extends Named {
 
 // class Person implements Greetable, Named {
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
 let user1: Greetable;
+let user2: Greetable;
 
 user1 = new Person('Max');
+user2 = new Person();
+
 // user1.name = 'Manuel'; // not allowed
 
 // user1 = {
@@ -33,6 +43,7 @@ user1 = new Person('Max');
 // };
 
 user1.greet('Hi there - I am');
+user2.greet('Hi there - I am');
 
 // type AddFn = (a: number, b: number) => number;
 interface AddFn {
