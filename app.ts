297711e38ab7ae25d08c805cc2aa52b439686a6b
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-16 18:09:45 +0100

    Discriminated Unions

--- app.ts
+++ app.ts
@@ -96,3 +96,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Could be classes as well
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSPeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed: number;
+
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSPeed;
+  }
+
+  console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
