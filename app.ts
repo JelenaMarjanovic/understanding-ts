5ecd962670c82bfd480ae503818e0fea6bffc13e
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-16 17:46:28 +0100

    More on Types Guards

--- app.ts
+++ app.ts
@@ -38,3 +38,61 @@ type Universal = Combinable & Numeric;
 const uni: Universal = 2020;
 
 console.log(uni);
+
+function add(a: Combinable, b: Combinable) {
+  // Type Guard
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString;
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo ... ' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  // if ('loadCargo' in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
