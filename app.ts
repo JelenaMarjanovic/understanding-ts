87888bba46b080e8743a8d993089c05383a634bb
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-20 23:09:57 +0100

    Creating a Generic Function

--- app.ts
+++ app.ts
@@ -1,14 +1,23 @@
-// const names: string[] = [];
-const names: Array<string> = ['Max', 'Manuel'];
-names.map((name) => console.log(name));
+// // const names: string[] = [];
+// const names: Array<string> = ['Max', 'Manuel'];
+// names.map((name) => console.log(name));
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  const words = data.split(' ');
-  console.log(words);
-});
+// promise.then((data) => {
+//   const words = data.split(' ');
+//   console.log(words);
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Max' }, { age: 30 });
+
+console.log(mergedObj);
+console.log('Name: ' + mergedObj.name);
