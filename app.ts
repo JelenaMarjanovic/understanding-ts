4f2c23b7c0e755bf4a54804fd8c50cbfa2c25286
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-20 23:21:44 +0100

    Working with Constraints

--- app.ts
+++ app.ts
@@ -13,10 +13,11 @@
 //   console.log(words);
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
+// const mergedObj = merge({ name: 'Max' }, 30);
 const mergedObj = merge({ name: 'Max' }, { age: 30 });
 
 console.log(mergedObj);
