3d26e37fb4a2174c0c114f1fba0400cd9c5b5aa0
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-21 14:32:50 +0100

    Another Generic Function

--- app.ts
+++ app.ts
@@ -28,3 +28,22 @@ const mergedObj = merge({ name: 'Max' }, { age: 30 });
 
 console.log(mergedObj);
 console.log('Name: ' + mergedObj.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
