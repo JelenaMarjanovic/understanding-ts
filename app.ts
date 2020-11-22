3c9d4752c9181d2276559a7b5832feca270d0e43
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-22 15:39:54 +0100

    Working with Decorator Factories

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+// function Logger(constructor: Function) {
+//   console.log('Logging...');
+//   console.log(constructor);
+// }
+
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+// @Logger
+@Logger('LOGGING - PERSON')
 class DecoratedPerson {
   name = 'Max';
 
